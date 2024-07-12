16922929bd37672ead2779a6944a28dccbf83ae0
ZinThantWin <zinthantwin1997@gmail.com>
2024-07-12 14:19:22 +0630

    shorten code

--- Sources/NetworkMonitor/NetworkMonitor.swift
+++ Sources/NetworkMonitor/NetworkMonitor.swift
@@ -2,13 +2,12 @@ import SwiftUI
 import Network
 
 public class NetworkMonitor : ObservableObject {
-    @Published public var isConnected = false
+    @Published var isConnected = false
     private let workerQueue = DispatchQueue(label: "Monitor")
     private let networkMonitor = NWPathMonitor()
     
     public init (){
         networkMonitor.pathUpdateHandler = {path in
-            self.isConnected = path.status == .satisfied
             DispatchQueue.main.async {
                 self.isConnected = path.status == .satisfied
             }
